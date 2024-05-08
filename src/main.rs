da0bd6aa2aea3ec270b1e3ba0b5d45b1579dda1a
Rafael-Ramosc <rafael.ramosrc@gmail.com>
2024-05-08 11:32:32 -0300

    refactor: Update main.rs with TcpListener bind error handling and client connection management

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,13 @@
 use mio::net::TcpListener;
 use mio::{Events, Interest, Poll, Token};
 use std::collections::HashMap;
+use std::io::{Read, Write};
 use std::time::Duration;
 
 fn main() -> Result<(), std::io::Error> {
     let mut poll = Poll::new()?;
     let mut events = Events::with_capacity(1024);
-
-    // let mut clients = HashMap::new();
+    let mut clients = HashMap::new();
 
     let address = "127.0.0.1:8000".parse().unwrap();
     let mut listener = TcpListener::bind(address)?;
@@ -17,6 +17,8 @@ fn main() -> Result<(), std::io::Error> {
         .register(&mut listener, SERVER, Interest::READABLE)
         .unwrap();
 
+    let mut next_token = Token(1);
+
     loop {
         poll.poll(&mut events, Some(Duration::from_millis(100)))?;
 
@@ -24,14 +26,47 @@ fn main() -> Result<(), std::io::Error> {
             match event.token() {
                 SERVER => loop {
                     match listener.accept() {
-                        Ok((connection, address)) => {
+                        Ok((mut connection, address)) => {
                             println!("Accepted connection from: {}", address);
+
+                            let token = next_token;
+                            next_token.0 += 1;
+
+                            poll.registry()
+                                .register(&mut connection, token, Interest::READABLE)
+                                .unwrap();
+
+                            clients.insert(token, connection);
                         }
                         Err(ref err) if would_block(err) => break,
                         Err(err) => return Err(err),
                     }
                 },
-                _ => unreachable!(),
+                token => {
+                    let mut connection = clients.remove(&token).unwrap();
+
+                    loop {
+                        let mut buffer = [0; 1024];
+                        match connection.read(&mut buffer) {
+                            Ok(0) => {
+                                println!("Client disconnected");
+                                break;
+                            }
+                            Ok(n) => {
+                                println!("Received data: {:?}", &buffer[..n]);
+                                connection.write_all(&buffer[..n]).unwrap();
+                            }
+                            Err(ref err) if would_block(err) => {
+                                clients.insert(token, connection);
+                                break;
+                            }
+                            Err(err) => {
+                                println!("Error: {}", err);
+                                break;
+                            }
+                        }
+                    }
+                }
             }
         }
     }
